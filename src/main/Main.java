6bb0414c733dc99e6867d5fe466d56c4682edea9
Oliver Legg <oliglegg@hotmail.co.uk>
2019-04-01 22:36:55 +0100

    Adding comments and testing

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,9 +21,18 @@ public class Main {
 		// While the windows isn't closed print to the screen
 		while (!window.closed())
 		{
+			// Start update
 			window.update();
+			
+			// Testing to see if input is working
 			if (window.isKeyPressed(GLFW.GLFW_KEY_SPACE))
 				System.out.println("Hey");
+			
+			// Printing x and y coordinate on mouse clicks for testing
+			if (window.isMousePressed(GLFW.GLFW_MOUSE_BUTTON_LEFT))
+				System.out.println(window.getMouseX()+", "+window.getMouseY());
+			
+			// Finish update
 			window.swapBuffers();
 		}
 
