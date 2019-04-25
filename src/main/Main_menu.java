40c1a28015e4595181b2bec8321b839c7728ff62
Tom Coupe <thomas.coupe@yahoo.co.uk>
2019-04-25 20:40:28 +0100

    added a creators page with links to github and website

--- src/main/Main_menu.java
+++ src/main/Main_menu.java
@@ -1,6 +1,12 @@
 package main;
 
 import org.lwjgl.glfw.GLFW;
+
+import com.sun.jndi.toolkit.url.Uri;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import engine.io.Audio;
 import gui.Button;
 import gui.Cloud;
@@ -8,6 +14,10 @@ import gui.Texture;
 import level.handler.Load;
 import playing.Playing;
 import gui.Slider;
+
+import java.awt.Desktop;
+import java.awt.desktop.*;
+import java.io.IOException;
 
 /**
  * @author Team 62
@@ -25,11 +35,9 @@ public class Main_menu {
 	public static final int ABOUT = 5;
 	public static final int EXIT = 6;
 	public static int state = 0;
-	private static Button[] buttons = new Button[5];
-	private static Texture title, background, howtoplay;
+	private static Button[] buttons = new Button[6];
+	private static Texture title, background, howtoplay, creators;
 	private static Cloud clouds[] = new Cloud[2];
-	private static int counter = 0;
-
 	private static float boxW = 580;
 	private static float boxH = 580; 
 	private static float boxX = (Main.window.getWidth() / 2) - (boxW/2);
@@ -37,14 +45,18 @@ public class Main_menu {
 	private static Button backToMenu = new Button("Back", 580, 535, 100, 50, MAIN);
 	public static Button vsync = new Button("Vsync", 150, 400, 170, 60, MAIN);
 	public static Button mute = new Button("Toggle Mute", 480 ,400, 170, 60, MAIN);
+	public static Button github = new Button("GitHub", 150, 400, 170, 60, MAIN);
+	public static Button website = new Button("Website", 480 ,400, 170, 60, MAIN);
 	public static Button fullscreen = new Button("Fullscreen", 340,400,120,60,MAIN);
 	public static Slider volume_sfx = new Slider("Sound Effects:", 500, 50, 150, 125, 50);
 	public static Slider volume_music = new Slider("Music: ",500, 50, 150, 225, 50);
 	public static Slider max_fps = new Slider("FPS:", 500, 50, 150, 325, 500);
+	private static Desktop d = Desktop.getDesktop();
 	
 	
 	public static void create() {
 		background = new Texture("background_small.jpg", 0, 0, 1f, 1f);
+		creators = new Texture("creators.png", 132, 50, 1f ,1f);
 		title = new Texture("turris_text.png", 283, 25, 1f, 1f);
 		howtoplay = new Texture("howtoplay.png", boxX, boxY, 1f, 1f);
 		for (int i = 0; i < clouds.length; i++){
@@ -56,8 +68,8 @@ public class Main_menu {
 		float b_h = 50;
 		float b_yIncrement = 60;
 		
-		String[]names ={"New Game","Load Game","Settings","How To Play","Exit"};
-		int[]   states={ NEW_GAME,  LOAD_GAME,  SETTINGS,  HELP,         EXIT };
+		String[]names ={"New Game","Load Game","Settings","How To Play","About","Exit"};
+		int[]   states={ NEW_GAME,  LOAD_GAME,  SETTINGS,  HELP,         ABOUT,  EXIT };
 		
 		for (int i = 0; i < buttons.length; i++){
 			buttons[i] = new Button(names[i], b_x, b_y, b_w, b_h, states[i]);
@@ -68,7 +80,7 @@ public class Main_menu {
 		
 	}
 	
-	public static void update(double dt)
+	public static void update(double dt) 
 	{
 		for (Cloud cloud : clouds) cloud.update(dt);
 		switch (state){
@@ -99,9 +111,30 @@ public class Main_menu {
 				}
 				break;
 			case ABOUT:
-				if (Main.window.isKeyReleased(GLFW.GLFW_KEY_ESCAPE)) {
+				if (backToMenu.updateClick()) {
 					state = MAIN;
 				}
+				if(github.updateClick()) {
+					if (Desktop.isDesktopSupported() && d.isSupported(Desktop.Action.BROWSE)) {
+						try {
+							d.browse(new URI("https://github.com/OleeL/Turris"));
+						} catch (IOException | URISyntaxException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+				if(website.updateClick()) {
+					if (Desktop.isDesktopSupported() && d.isSupported(Desktop.Action.BROWSE)) {
+						try {
+							d.browse(new URI("https://student.csc.liv.ac.uk/~sgkbaker/Turris.html"));
+						} catch (IOException | URISyntaxException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+				
 				break;
 			case SETTINGS:
 				volume_music.setEnabled(true);
@@ -221,8 +254,14 @@ public class Main_menu {
 				backToMenu.setFontColour(1f, 1f, 1f);
 				backToMenu.draw();
 				break;
-				
-				
+			case ABOUT:
+				Main.window.setColour(0f, 0f, 0f, 0.5f);
+				Main.window.rectangle(boxX, boxY, boxW, boxH, 20);
+				creators.draw();
+				github.draw();
+				website.draw();
+				backToMenu.setFontColour(1f, 1f, 1f);
+				backToMenu.draw();	
 		}
 	}
 	
