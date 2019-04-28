858db6d88820d18da71a672d79fcb1e4fda3e846
Oliver Legg <oliglegg@hotmail.co.uk>
2019-04-28 12:36:40 +0100

    Fixed showing in the wrong texture on enemy 3

--- src/enemies/Enemy_3.java
+++ src/enemies/Enemy_3.java
@@ -13,7 +13,7 @@ public class Enemy_3 extends Enemy{
 	
 	static {
 		for (int i = 0; i < 3; i++) {
-			Texture texture = new Texture("enemies/enemy_2/"+i+".png",0,0,1,1);
+			Texture texture = new Texture("enemies/enemy_3/"+i+".png",0,0,1,1);
 			textures.put("3"+i, texture);
 		}
 	}
@@ -21,7 +21,7 @@ public class Enemy_3 extends Enemy{
 	public Enemy_3(float x, float y, float grid_size) {
 		super((byte) 3, x, y, grid_size);
 		health = 200;
-		value = 15;
+		value = 10;
 		speed = 3;
 	}
 	
